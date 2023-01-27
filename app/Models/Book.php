a394026dbdd5ef37e4ba45b0cf57770090ce4176
adrianochristian <adriano.sousa@alfasoft.pt>
2023-01-27 04:15:19 -0300

    [wip] validation rules and model progress

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,14 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'books';
 
-    public $fillable = ['title','pages','publication_date','publisher_id'];
+    public $fillable = ['title','pages','published_at','publisher_id'];
+
+    protected $dates = ['created_at','updated_at','deleted_at','published_at'];
+
+    public function rules(){
+        return 
+        [
+            'title' => 'required',
+            'pages' => 'required',
+            'publisher_id' => 'required',
+            'published_at' => 'required',
+        ];
+    } 
     
     public function publisher(){
         return $this->belongsTo(Publisher::class);
